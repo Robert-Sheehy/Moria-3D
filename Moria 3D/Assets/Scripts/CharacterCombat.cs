77210f459ea629600d3aafd01766b26fdf20e887
Jamie Hennigar <jamiehennigar@gmail.com>
2016-10-05 10:50:49 +0100

    character combat script and combat system script

---
+++ Moria 3D/Assets/Scripts/CharacterCombat.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CharacterCombat : MonoBehaviour {
+    internal int weaponDamage;
+    internal int critcalBonus;
+    internal int weaponDamageBonus;
+    internal int itemBonusDamage;
+    internal int strBonusDamage;
+    internal int weaponHitBonus;
+    internal int itemHitBonus;
+    internal int fightingAbility;
+    internal int weightOfWeapon;
+    internal int fightingAbilityAt0;
+    internal int hitChance;
+    internal int DEXHitBonus;
+    internal int STRHitBonus;
+    private int enemyHealth;
+    private int playerHealth;
+    public GameObject player;
+    public GameObject enemy;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+	}
+
+    internal void ApplyDamage(int totalDamage)
+    {
+        
+            
+        enemyHealth += enemyHealth - totalDamage;
+    }
+
+    internal void applyHit(int hitChance)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void applyCrit(int critChance)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Moria 3D/Assets/Scripts/CombatSystem.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatSystem : MonoBehaviour {
+    private int randomHitNumber;
+    private int monsterAC;
+    private int randomHitNumber2;
+    private int hitChance;
+    private int criticalBonus;
+    private int randomCritNumber = Random.Range(1, 5000);
+    
+
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+    public void Attack(CharacterCombat attacker,CharacterCombat victim)
+    {
+
+
+        int  totalDamage = (attacker.weaponDamage  * attacker.critcalBonus) + attacker.weaponDamageBonus + attacker.itemBonusDamage + attacker.strBonusDamage;
+        victim.ApplyDamage(totalDamage);
+    }
+
+    public void Hit(CharacterCombat attacker, CharacterCombat victim)
+    {
+
+        randomHitNumber = Random.Range(1, 20);
+        int hitChance = (attacker.weaponHitBonus + attacker.itemHitBonus + attacker.DEXHitBonus + attacker.STRHitBonus) * 3 + attacker.fightingAbility;
+        randomHitNumber2 = Random.Range(1, hitChance);
+
+        if (randomHitNumber == 1)
+        {
+            print("you missed");
+        }
+        else if ((randomHitNumber == 20) || (randomHitNumber2 > monsterAC))
+        {
+            Attack(attacker, victim);
+        }
+    }
+
+    public int CritialHitBonus(CharacterCombat attacker, CharacterCombat victim)
+    {
+        //int randomCritNumber = Random.Range(1, 5000);
+        if (randomCritNumber < criticalBonus)
+        {
+            return (attacker.weaponHitBonus + attacker.itemHitBonus + attacker.DEXHitBonus + attacker.STRHitBonus) * 5 + attacker.fightingAbility + (attacker.weightOfWeapon) * 10 - attacker.fightingAbilityAt0;
+        }
+        else
+            print("denied");
+         return 1;
+    }
+}
